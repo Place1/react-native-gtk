61df4b0ba4b5cbcb071a3f9d4d7326f4d65bef87
Jam Risser <jam@jamrizzi.com>
2017-10-20 00:46:00 -0500

    Added all button events

--- packages/gtk-node/src/Button.hpp
+++ packages/gtk-node/src/Button.hpp
@@ -12,7 +12,11 @@ using namespace std;
 
 class Button : public Widget {
 BASIC_WIDGET(Gtk::Button, Button)
+DEFINE_EVENT(onRelease)
 DEFINE_EVENT(onClick)
+DEFINE_EVENT(onEnter)
+DEFINE_EVENT(onLeave)
+DEFINE_EVENT(onActivate)
 DEFINE_GETTER(string, getLabel)
 DEFINE_SETTER(string, setLabel)
 
